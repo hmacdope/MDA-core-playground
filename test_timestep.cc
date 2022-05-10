840c64f0778132b0739ae6f2bd322b53d30dd438
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2022-05-10 16:33:44 +1000

    more

--- test_timestep.cc
+++ test_timestep.cc
@@ -1,9 +1,13 @@
 
 #include "Timestep.h"
+#include "Dimensions.h"
 
 int main(){
 
-auto tstep = mdacore::Timestep<float>(10);
+float boxmat[6]  = {0,1,2,3,4,5};
+
+auto tstep = mdacore::Timestep<float, mdacore::OrthogonalDimensions<float>>(10, boxmat);
+
 tstep.DebugPrint();
 
 
