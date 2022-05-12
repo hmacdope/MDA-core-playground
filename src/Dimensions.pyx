46d2044df0730cb9251903d74270e2ad3f986fe5
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2022-05-12 14:20:44 +1000

    improve dimensions.pyx

--- src/Dimensions.pyx
+++ src/Dimensions.pyx
@@ -1,19 +1,25 @@
 # distutils: language = c++
 
 from _Dimensions cimport OrthogonalDimensions
+from libcpp.vector cimport vector
+ 
 
 
 cdef class OrthogonalDimensions_F:
     cdef OrthogonalDimensions[float] _OrthogonalDimensions
+    cdef float* _box_ptr
     
-    def __cinit__(self, list box):
-        self._OrthogonalDimensions = OrthogonalDimensions[float](box)
+    def __cinit__(self, vector[float] vec ):
+        self._OrthogonalDimensions = OrthogonalDimensions[float](vec)
+        self._box_ptr = self._OrthogonalDimensions.box.data()
 
     @property
     def box(self):
         return self._OrthogonalDimensions.box
     
     @box.setter
-    def box(self, box):
-        self._OrthogonalDimensions.box = box
+    def box(self, list newbox):
+        self._OrthogonalDimensions.box = newbox
+
+
 
