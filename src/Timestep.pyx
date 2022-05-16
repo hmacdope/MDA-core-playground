3893a0f047b49658afbd0e9bedea714cd8f50666
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2022-05-16 13:21:07 +1000

    improve again

--- src/Timestep.pyx
+++ src/Timestep.pyx
@@ -43,8 +43,10 @@ cdef class TimestepContainer:
     cdef dimensions_ptr_t _Dimensions_ptr
     cdef arr_type_t _dimtype
     cdef arr_type_t _pd_type
+    cdef void* _danger_ptr
     cdef timestep_type_t _timestep_type
     cdef uint64_t n_atoms
+
 
 
     def __cinit__(self, uint64_t n_atoms, position_dependent_dtype=np.float32, dimensions_dtype=np.float64):
@@ -52,13 +54,13 @@ cdef class TimestepContainer:
             self.self._pd_type = arr_type_t.FLOAT
             if  dimensions_dtype == np.float64: # most common is FLOAT_DOUBLE so put first
                 self._dimtype = arr_type_t.DOUBLE
-                self._Timestep_ptr.float_double_ptr = new Timestep[float, double](n_atoms, box)
+                self._Timestep_ptr.float_double_ptr = new Timestep[float, double](n_atoms)
                 self._timestep_type = timestep_type_t.FLOAT_DOUBLE
                 self._Dimensions_ptr.double_ptr =  &dereference(self._Timestep_ptr.float_double_ptr).unitcell
 
             elif dimensions_dtype == np.float32:
                 self._dimtype = arr_type_t.FLOAT # FLOAT_FLOAT
-                self._Timestep_ptr.float_float_ptr = new Timestep[float, float](n_atoms, box)
+                self._Timestep_ptr.float_float_ptr = new Timestep[float, float](n_atoms)
                 self._timestep_type = timestep_type_t.FLOAT_FLOAT
                 self._Dimensions_ptr.float_ptr =  &dereference(self._Timestep_ptr.float_float_ptr).unitcell
         
@@ -66,13 +68,13 @@ cdef class TimestepContainer:
             self.self._pd_type = arr_type_t.DOUBLE
             if  dimensions_dtype == np.float64:  # DOUBLE_DOUBLE
                 self._dimtype = arr_type_t.DOUBLE
-                self._Timestep_ptr.double_double_ptr = new Timestep[double, double](n_atoms, box)
-                self._timestep_type = timestep_type_t.DOBLE_DOUBLE
+                self._Timestep_ptr.double_double_ptr = new Timestep[double, double](n_atoms)
+                self._timestep_type = timestep_type_t.DOUBLE_DOUBLE
                 self._Dimensions_ptr.double_ptr =  &dereference(self._Timestep_ptr.double_double_ptr).unitcell
 
             elif dimensions_dtype == np.float32:
                 self._dimtype = arr_type_t.FLOAT # DOUBLE_FLOAT
-                self._Timestep_ptr.double_float_ptr = new Timestep[double, float](n_atoms, box)
+                self._Timestep_ptr.double_float_ptr = new Timestep[double, float](n_atoms)
                 self._timestep_type = timestep_type_t.DOUBLE_FLOAT
                 self._Dimensions_ptr.float_ptr =  &dereference(self._Timestep_ptr.float_float_ptr).unitcell
 
@@ -99,8 +101,7 @@ cdef class TimestepContainer:
     
     @positions.setter
     def positions(self,  cnp.ndarray[cnp.float32_t, ndim=2] newpos):
-        cdef temp = newpos.flatten()
-        dereference(self._Timestep_ptr.float_float_ptr).SetPositions(temp)
+        dereference(self._Timestep_ptr.float_float_ptr).SetPositions(newpos.flatten())
 
     @property
     def dimensions(self):
@@ -110,12 +111,13 @@ cdef class TimestepContainer:
         return box_ndarr
     
     @dimensions.setter
-    def dimensions(self, cnp.ndarray[cnp.float32_t, ndim=1] newbox):
+    def dimensions(self, cnp.ndarray[cnp.float32_t, ndim=2] newbox):
         if newbox.ndim  >= 2:
             raise ValueError("box cannot be set with multidimensional array")
         cdef size_t first_dim =  newbox.shape[0]
-        if first_dim > self._Dimensions.max_size:
+        if first_dim > dereference(self._Dimensions_ptr.float_ptr).max_size:
             raise ValueError("box cannot be set with first dimension shape {}".format(first_dim))
-        self._Dimensions.box = newbox
+        dereference(self._Dimensions_ptr.float_ptr).box = newbox.flatten()
+
 
 
--- src/_Timestep.pxd
+++ src/_Timestep.pxd
@@ -9,7 +9,7 @@ from _Dimensions cimport Dimensions
 cdef extern from "Timestep.h" namespace "mdacore":
     cdef cppclass Timestep[T,U]:
         # how do I pass through the T  of U through
-        Timestep(const uint64_t n_atoms, const vector[U] &box)
+        Timestep(const uint64_t n_atoms)
         void SetPositions(const vector[T] &pos) 
         void SetVelocities(const vector[T] &vel)
         void SetForces(const vector[T] &frc)
